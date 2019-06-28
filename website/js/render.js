58e5ae39f8aa442e22bb1ac0664b9a3cf82bf23c
MahamdiAmine <fm_mahamdi@esi.dz>
2019-06-28 14:26:28 +0100

    aminov : fix the UI

--- website/js/render.js
+++ website/js/render.js
@@ -1,3 +1,14 @@
 function webCam() {
     eel.webCam();
 }
+
+function configs() {
+    let weightsPath = document.getElementById("weightsPath").value;
+    let outputPath = document.getElementById("outputPath").value;
+    let confidence = document.getElementById("confidence").value;
+    let threshold = document.getElementById("threshold").value;
+    let checkBox = document.getElementById("showTime");
+    let showTime = checkBox.checked;
+    eel.update_params(weightsPath, outputPath, confidence, threshold, showTime);
+
+}
